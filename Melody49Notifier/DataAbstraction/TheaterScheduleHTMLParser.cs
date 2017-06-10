7d9442b2f997c940f6d7bb88973f5c2cf9209b18
James Schafer <james@jschafer.net>
2017-06-10 19:18:11 -0400

    Successfully Parsed Data from the Theater Schedule HTML.

--- Melody49Notifier/DataAbstraction/TheaterScheduleHTMLParser.cs
+++ Melody49Notifier/DataAbstraction/TheaterScheduleHTMLParser.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Melody49Notifier.Models;
 using Microsoft.Azure.WebJobs.Host;
+using HtmlAgilityPack;
 
 namespace Melody49Notifier.DataAbstraction
 {
@@ -19,7 +20,16 @@ namespace Melody49Notifier.DataAbstraction
 
         public TheaterSchedule ParseTheaterScheduleHTML(string html)
         {
-            throw new NotImplementedException();
+            HtmlDocument htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            TheaterSchedule theaterSchedule = new TheaterSchedule();
+
+            //theaterSchedule.ScheduleDescription = htmlDocument.DocumentNode.Descendants("div").Where(x => x?.Attributes?["id"]?.Value == "playweek").First().ChildNodes.First().InnerText;
+
+            theaterSchedule.ScheduleDescription = htmlDocument.DocumentNode.SelectNodes("//div[@id=\"playweek\"]/p").First().InnerText;
+
+            return theaterSchedule;
         }
     }
 }
