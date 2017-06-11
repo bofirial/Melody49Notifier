caa164f2d43dbd3c4d2926256ddf3cfb9f704cf7
James Schafer <james@jschafer.net>
2017-06-10 22:23:01 -0400

    Implemented the TheaterScheduleComparer

--- Melody49Notifier/Models/TheaterScheduleComparer.cs
+++ Melody49Notifier/Models/TheaterScheduleComparer.cs
@@ -18,7 +18,21 @@ namespace Melody49Notifier.Models
 
         public bool AreEqual(TheaterSchedule firstTheaterSchedule, TheaterSchedule secondTheaterSchedule)
         {
-            throw new NotImplementedException();
+            bool areEqual = true;
+
+            areEqual = areEqual && firstTheaterSchedule.ScheduleDescription == secondTheaterSchedule.ScheduleDescription;
+            areEqual = areEqual && firstTheaterSchedule.TheaterName == secondTheaterSchedule.TheaterName;
+            areEqual = areEqual && firstTheaterSchedule.Showings.Count == secondTheaterSchedule.Showings.Count;
+
+            for (int i = 0; i < firstTheaterSchedule.Showings.Count; i++)
+            {
+                areEqual = areEqual && firstTheaterSchedule.Showings[i].MovieDescription == secondTheaterSchedule.Showings[i].MovieDescription;
+                //areEqual = areEqual && firstTheaterSchedule.Showings[i].Screen == secondTheaterSchedule.Showings[i].Screen;
+                //areEqual = areEqual && firstTheaterSchedule.Showings[i].ShowingScheduleDescription == secondTheaterSchedule.Showings[i].ShowingScheduleDescription;
+                //areEqual = areEqual && firstTheaterSchedule.Showings[i].ActorDescription == secondTheaterSchedule.Showings[i].ActorDescription;
+            }
+
+            return areEqual;
         }
     }
 }
