ab47a013859838cfd7ee94bee9ea1b6ea4b6c191
James Schafer <james@jschafer.net>
2017-06-11 09:01:30 -0400

    Created Initial NotificationEmailGenerator for reading a populating an EmailTemplate.

---
+++ Melody49Notifier/Notification/INotificationEmailGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Melody49Notifier.Models;
+
+namespace Melody49Notifier.Notification
+{
+    public interface INotificationEmailGenerator
+    {
+        string CreateFromTemplate(TheaterSchedule currentTheaterSchedule);
+    }
+}
---
+++ Melody49Notifier/Notification/NotificationEmailGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Melody49Notifier.Models;
+using Microsoft.Azure.WebJobs.Host;
+using System.IO;
+
+namespace Melody49Notifier.Notification
+{
+    public class NotificationEmailGenerator : INotificationEmailGenerator
+    {
+        public NotificationEmailGenerator(TraceWriter log)
+        {
+            Log = log;
+        }
+
+        public TraceWriter Log { get; }
+
+        public string CreateFromTemplate(TheaterSchedule currentTheaterSchedule)
+        {
+            return File.ReadAllText("EmailTemplate.html");
+        }
+    }
+}
