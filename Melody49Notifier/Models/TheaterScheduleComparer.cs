450eae212d38c08822c7789d5a4f6851876728e1
James Schafer <james@jschafer.net>
2017-06-10 22:27:20 -0400

    Added Null Checking for the TheaterScheduleComparer

--- Melody49Notifier/Models/TheaterScheduleComparer.cs
+++ Melody49Notifier/Models/TheaterScheduleComparer.cs
@@ -20,16 +20,16 @@ namespace Melody49Notifier.Models
         {
             bool areEqual = true;
 
-            areEqual = areEqual && firstTheaterSchedule.ScheduleDescription == secondTheaterSchedule.ScheduleDescription;
-            areEqual = areEqual && firstTheaterSchedule.TheaterName == secondTheaterSchedule.TheaterName;
-            areEqual = areEqual && firstTheaterSchedule.Showings.Count == secondTheaterSchedule.Showings.Count;
+            areEqual = areEqual && firstTheaterSchedule?.ScheduleDescription == secondTheaterSchedule?.ScheduleDescription;
+            areEqual = areEqual && firstTheaterSchedule?.TheaterName == secondTheaterSchedule?.TheaterName;
+            areEqual = areEqual && firstTheaterSchedule?.Showings?.Count == secondTheaterSchedule?.Showings?.Count;
 
-            for (int i = 0; i < firstTheaterSchedule.Showings.Count; i++)
+            for (int i = 0; i < firstTheaterSchedule?.Showings?.Count; i++)
             {
-                areEqual = areEqual && firstTheaterSchedule.Showings[i].MovieDescription == secondTheaterSchedule.Showings[i].MovieDescription;
-                //areEqual = areEqual && firstTheaterSchedule.Showings[i].Screen == secondTheaterSchedule.Showings[i].Screen;
-                //areEqual = areEqual && firstTheaterSchedule.Showings[i].ShowingScheduleDescription == secondTheaterSchedule.Showings[i].ShowingScheduleDescription;
-                //areEqual = areEqual && firstTheaterSchedule.Showings[i].ActorDescription == secondTheaterSchedule.Showings[i].ActorDescription;
+                areEqual = areEqual && firstTheaterSchedule?.Showings?[i]?.MovieDescription == secondTheaterSchedule?.Showings?[i]?.MovieDescription;
+                //areEqual = areEqual && firstTheaterSchedule?.Showings?[i]?.Screen == secondTheaterSchedule?.Showings?[i]?.Screen;
+                //areEqual = areEqual && firstTheaterSchedule?.Showings?[i]?.ShowingScheduleDescription == secondTheaterSchedule?.Showings?[i]?.ShowingScheduleDescription;
+                //areEqual = areEqual && firstTheaterSchedule?.Showings?[i]?.ActorDescription == secondTheaterSchedule?.Showings?[i]?.ActorDescription;
             }
 
             return areEqual;
